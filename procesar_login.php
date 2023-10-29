9b8e7649378e9b676337f48f0688c33352bb3f5c
N0oahs-C0 <N0oahs.5jp@gmail.com>
2023-10-28 18:23:18 -0600

    Secambiotododeubicacionxd

--- procesar_login.php
+++ procesar_login.php
@@ -1,24 +1,24 @@
 <?php
 // Conectar a la base de datos y validar las credenciales aquí
 //require("conexion.php");
-// session_start();
-// require("conexion.php");
-// $usuario = $_POST['user'];
-// $password = $_POST['pass'];
+session_start();
+require("conexion.php");
+$usuario = $_POST['user'];
+$password = $_POST['pass'];
 
 // // Validar usuario y contraseña en la base de datos
-// $query = "SELECT * FROM Login WHERE Usuario = '$usuario' AND Contrasena = '$password' AND Permisos = 'admin'";
-// $result = mysqli_query($conn, $query);
+$query = "SELECT * FROM USERS WHERE nombre = '$usuario' AND pass = '$password' AND Permisos = 'admin'";
+$result = mysqli_query($conn, $query);
 
-// if (!$result) {
-//     die("Error en la consulta: " . mysqli_error($conn));
-// }
+if (!$result) {
+     die("Error en la consulta: " . mysqli_error($conn));
+}
 
-// if (mysqli_num_rows($result) == 1) {
+if (mysqli_num_rows($result) == 1) {
 //     // Las credenciales son correctas
-//     $_SESSION['user'] = $_POST['user'];
-//     header('Location: seleccionarEmpleado.php');
-//     exit;
-// } else {
-//     echo "Error: Usuario o contraseña incorrectos.";
-// }
+   $_SESSION['user'] = $_POST['user'];
+   header('Location: seleccionarEmpleado.php');
+   exit;
+} else {
+    echo "Error: Usuario o contraseña incorrectos.";
+}
